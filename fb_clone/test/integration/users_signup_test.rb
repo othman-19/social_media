6408251e4d9913373b5b6f2eab5ec7acaa4cd4db
Mesi <mse212002@gmail.com>
2019-09-16 19:16:45 +0300

    Adding User model, Post scaffold,bootstrap gem and integration test for user

---
+++ fb_clone/test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password: "foo" } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
