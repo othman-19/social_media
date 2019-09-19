dcf767c395b10622a31ec4a6179c49873fb313d0
othmane <othman-19@hotmail.fr>
2019-09-19 19:45:28 +0100

    customize messages css design,change routes configurations

--- fb_clone/config/routes.rb
+++ fb_clone/config/routes.rb
@@ -1,28 +1,24 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users, only: [:new, :show]
+  resources :users, only: [:new, :show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  devise_for :users, skip: [:sessions]
+  devise_for :users, path: 'auth', path_names: {sign_in: 'login',
+                                                sign_out: 'logout',
+                                                password: 'secret',
+                                                confirmation: 'verification',
+                                                unlock: 'unblock',
+                                                registration: 'register',
+                                                sign_up: 'cmon_let_me_in' }
 
-    
-    devise_scope :user do
+  devise_scope :user do
       authenticated :user do
          root 'posts#index', as: :authenticated_root
       end
-    end
+    
 
     unauthenticated do
       root 'devise/registrations#new', as: :unauthenticated_root
     end
-
-    as :user do
-      get 'login', to: 'devise/sessions#new', as: :new_user_session
-      post 'login', to: 'devise/sessions#create', as: :user_session
-      match 'signout', to: 'devise/sessions#destroy',
-                       as: :destroy_user_session,
-                       via: Devise.mappings[:user].sign_out_via
-      get 'signup', to: 'devise/registrations#new'
-    end
   end
-  
+end
