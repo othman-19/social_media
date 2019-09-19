bd92a0e9cc9c11dadbb0b058c5b9c3ecbc8e035a
othmane <othman-19@hotmail.fr>
2019-09-19 12:40:26 +0100

    add devise user controller, customize devise forms css design, set routes,root and links, set authentication test

--- fb_clone/config/routes.rb
+++ fb_clone/config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  resources :users, only: [:new, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-end
+  devise_for :users, skip: [:sessions]
+
+    
+    devise_scope :user do
+      authenticated :user do
+         root 'posts#index', as: :authenticated_root
+      end
+    end
+
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+
+    as :user do
+      get 'login', to: 'devise/sessions#new', as: :new_user_session
+      post 'login', to: 'devise/sessions#create', as: :user_session
+      match 'signout', to: 'devise/sessions#destroy',
+                       as: :destroy_user_session,
+                       via: Devise.mappings[:user].sign_out_via
+      get 'signup', to: 'devise/registrations#new'
+    end
+  end
+  
--- fb_clone/test/integration/users_signup_test.rb
+++ fb_clone/test/integration/users_signup_test.rb
@@ -3,23 +3,24 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
-    get signup_path
+    get new_user_registration_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post user_registration_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password: "foo" } }
     end
-    assert_template 'users/new'
+    assert_template "devise/shared/_error_messages"
+    assert_template "devise/shared/_links"
+    assert_template "devise/registrations/new"
+    assert_template "layouts/application"
   end
 
   test "valid signup information" do
-    get signup_path
+    get new_user_registration_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
+      post user_registration_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password: "password" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
   end
 end
