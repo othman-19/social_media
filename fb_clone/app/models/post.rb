9fd9b967890c38da8781fefddd770ef67ddba252
othmane <othman-19@hotmail.fr>
2019-09-19 19:57:44 +0100

    Add User and Post validations

--- fb_clone/app/models/post.rb
+++ fb_clone/app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
+    
+    default_scope -> { order(created_at: :desc) }
+
+    validates :user_id, presence: true
+    validates :content, presence: true, length: { maximum: 140 }
 end
