533e50258080c904bb4c7f38bb9538312eb041d8
othmane <othman-19@hotmail.fr>
2019-09-23 13:48:46 +0100

    Allow post to upload images, populate database with gem faker in seeds

--- fb_clone/db/schema.rb
+++ fb_clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_113344) do
+ActiveRecord::Schema.define(version: 2019_09_23_092557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_21_113344) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
