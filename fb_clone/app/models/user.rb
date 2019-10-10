860fee35f3c46fb11d2d11e6b5fc2e892751433d
othmane <othman-19@hotmail.fr>
2019-10-10 10:11:42 +0100

    reset the friendship feature

--- fb_clone/app/models/user.rb
+++ fb_clone/app/models/user.rb
@@ -11,9 +11,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-
   before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }, allow_blank: false
