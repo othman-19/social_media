5b283e386028596df48825ff24effd7bdced2386
stickler-ci <support@stickler-ci.com>
2019-10-16 11:13:41 +0000

    Fixing style errors.

--- fb_clone/spec/features/friend_request/create_friend_request_spec.rb
+++ fb_clone/spec/features/friend_request/create_friend_request_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 # creating friend request.
 RSpec.describe 'Friend request', type: :feature do
-  let(:user1){ User.create( name: 'username1', email: 'useremail1@gmail.com', password: 'password') }
-  let(:friend1){ User.create( name: 'friendname1', email: 'friendemail1@gmail.com', password: 'password') }
+  let(:user1) { User.create(name: 'username1', email: 'useremail1@gmail.com', password: 'password') }
+  let(:friend1) { User.create(name: 'friendname1', email: 'friendemail1@gmail.com', password: 'password') }
   scenario 'creating friend request' do
     def create_request
       user1.friend_requests.create(friend: friend1)
