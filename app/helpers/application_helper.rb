a15bc952a1a0182e89706314a7ef01614825b087
othmane <othman-19@hotmail.fr>
2019-10-25 18:40:40 +0100

    add friend reques notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def notification(user)
+  def friend_request_notifications(user)
     FriendRequest.where(friend: user).count
   end
 end
--- app/helpers/friend_requests_helper.rb
+++ app/helpers/friend_requests_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 module FriendRequestsHelper
-  def notification(user)
-    FriendRequest.where(friend: user).count
-  end
+  
 end
