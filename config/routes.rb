7c0f8294d9772d52f24411d3fb8cffb2db99ae2f
othmane <othman-19@hotmail.fr>
2019-11-02 10:39:18 +0100

    reset the omniauth configuration

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: { registrations: 'users/registrations',
-                                    omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  devise_for :users, controllers: { registrations: 'users/registrations'},
+                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   as :user do
     authenticated :user do
       root 'posts#index', as: :authenticated_root
