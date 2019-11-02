b0bc80ca15366b7682ceb429a3c9e547e1f54ee1
othmane <othman-19@hotmail.fr>
2019-11-02 12:26:27 +0100

    change application Id ans callback url

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,9 +261,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, '2662261970683547',
-                             'aa2154623cd6f87a849c5181ac3c9209',
-                              callback_url: 'https://pacific-dusk-68520.herokuapp.com/users/auth/facebook/callback'
+  config.omniauth :facebook, '401307487207712',
+                             'bee8c32ea6e46d6b56fb30990db26b7d',
+                              callback_url: 'https://polar-oasis-98766.herokuapp.com/users/auth/facebook/callback'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
